79b2c9e9a9742b2c2ade7b316954e0a2d1edd31d
Stephen Bates <Stephen.Michael.Bates@outlook.com>
2024-01-27 22:04:36 -0500

    refactored javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
+// Define HTML elements to be referenced
+var headerEl = document.querySelector('header');
+var highscoreTable = document.querySelector('#highscore-table');
+var timerEl = document.querySelector('#timer');
+
+var mainEl = document.querySelector('main');
+
 // Key to localstorage to access highscore data
 const highscoreKey = 'highscores';
+// List of recorded highscores
+var highscores = [];
 // Duration of the quiz in seconds
 const quizDuration = 60;
 // Pool of potential questions to be asked
@@ -12,7 +21,8 @@ const questions = [
             "The other",
             "All of the above"
         ],
-        correct: 3
+        correctIndex: 3,
+        tip: "I don't Know"
     },
     {
         question: "Say what? Again",
@@ -22,213 +32,206 @@ const questions = [
             "The other, again",
             "All of the above, again"
         ],
-        correct: 3
+        correctIndex: 3,
+        tip: "I don't know, again"
     },
 ]
+// Pool of questions not yet asked for current quiz
 var unaskedQuestions = [];
+var currentQuestion;
+// Reference for interval and timeout used
+var quizInterval;
+var questionTimeout;
 
-// List of recorded highscores
-var highscores = [];
-
-var quizTimers = [];
-// Create <header> element and its children
-function createHeader(){
-    var header = document.createElement('header');
-    var highscoreTable = document.createElement('section');
-    var highscoreList = document.createElement('ol');
-    var timerSection = document.createElement('div');
-
-    highscoreTable.setAttribute('id','highscore-table');
-    highscoreTable.innerHTML = "Highscore Table";
-    highscoreList.setAttribute('id','highscore-list');
-    
-    timerSection.setAttribute('id', 'quiz-timer');
-
-    highscoreTable.appendChild(highscoreList);
-    header.appendChild(highscoreTable);
-    header.appendChild(timerSection);
-    document.body.appendChild(header);
-}
-
+// Create elements for the highscore table
 function updateHighscoreTable(){
-    var scoreItems = [];
-    for (const score of highscores) {
-        var scoreItem = document.createElement('li');
-        var scoreName = document.createElement('div');
-        var scoreValue = document.createElement('div');
+    var tableHeader = document.createElement('tr');
+    var nameHeader = document.createElement('th');
+    nameHeader.innerHTML = "Name";
+    tableHeader.appendChild(nameHeader);
+    var scoreHeader = document.createElement('th');
+    scoreHeader.innerHTML = "Score";
+    tableHeader.appendChild(scoreHeader);
 
+    highscoreTable.replaceChildren(tableHeader);
+
+    for (const score of highscores) {
+        var scoreItem = document.createElement('tr');
+
+        var scoreName = document.createElement('td');
         scoreName.innerHTML = score.name;
+
+        var scoreValue = document.createElement('td');
         scoreValue.innerHTML = score.value;
-        scoreName.classList.add('highscore-item-name');
-        scoreValue.classList.add('highscore-item-value');
-        
-        scoreItem.classList.add('highscore-item');
+
         scoreItem.appendChild(scoreName);
         scoreItem.appendChild(scoreValue);
 
-        scoreItems.push(scoreItem);
+        highscoreTable.appendChild(scoreItem);
     }
-    document.querySelector('#highscore-list').replaceChildren(...scoreItems);
 }
 
-function updateTimer(duration, elapsedTime){
-    document.querySelector('#quiz-timer').innerHTML = (duration - elapsedTime / 100).toPrecision(3);        
+function updateTimer(){
+    // Reduce time remaining each step
+    timerEl.dataset.timeRemaining--;
+    // Set timer html to render time remaining to screen (divide by 10 to convert back to full seconds)
+    timerEl.innerHTML = timerEl.dataset.timeRemaining / 10;
+    // If no more time remains
+    if (timerEl.dataset.timeRemaining <= 0){
+        // End the quiz
+        endQuiz()
+    }
 }
 
-// Create <main> element and its children
-function createBody(){
-    var main = document.createElement('main');
-    var textSection = document.createElement('section');
-    var actionSection = document.createElement('section');
-    
-    textSection.setAttribute('id','main-text-section');
-    actionSection.setAttribute('id','main-action-section');
-
-    main.appendChild(textSection);
-    main.appendChild(actionSection);
-    document.body.appendChild(main);
-}
-
-function mainIntro(){
+// Set up main intro scene
+function introScene(){
+    // Introduce user and give brief explination
+    var text = document.createElement('h2');
+    text.innerHTML = "Welcome to Quiz Time<br>Click the button to start!";
+    // Add button to start the quiz
     var button = document.createElement('button');
-
-    document.querySelector('#main-text-section').innerHTML = "Welcome to Quiz Time<br>Click the button to start!";
     button.innerHTML = "Start";
-
     button.addEventListener('click', function(){
         startQuiz();
     })
-    document.querySelector('#main-action-section').replaceChildren(button);
+    // Clear action section and set up for intro
+    return [text,button];
 }
 
 function startQuiz(){
-    quizTimers.push(startTimer(quizDuration, 10, updateTimer, endQuiz));
+    // Set timer data to quiz duration (multiply by 10 for tenths of a second)
+    timerEl.dataset.timeRemaining = quizDuration * 10;
+    // Start an interval to update quiz timer every milliseconds
+    quizInterval = setInterval(function() {updateTimer();},100);
+    // Add all available questions to unasked list
     unaskedQuestions = [...questions];
-    nextQuestion();
+    
+    // TODO; implement check to make sure there were questions to start off with
+    // Prio; Low
+
+    // Begin asking questions
+    mainEl.replaceChildren(...nextQuestion());
 }
 
 function nextQuestion(){
-    // End the quiz if there are no unasked questions
-    if( unaskedQuestions.length == 0){
-        endQuiz();
-        return;
-    }
-    // Otherwise,
     // Select a random question from the questions that haven't been asked this round
-    
-    var question = unaskedQuestions[Math.floor(Math.random(unaskedQuestions.length))];
-    unaskedQuestions.splice(unaskedQuestions.indexOf(question), 1);
-    console.log(unaskedQuestions);
+    currentQuestion = unaskedQuestions[Math.floor(Math.random(unaskedQuestions.length))];
+    // Remove selected question from unasked list of questions
+    unaskedQuestions.splice(unaskedQuestions.indexOf(currentQuestion), 1);
     // Render the question to the screen
-    document.querySelector('#main-text-section').innerHTML = question.question;
+    var text = document.createElement('h2');
+    text.innerHTML = currentQuestion.question;
     // Create the list that will hold the anwsers
-    var anwserList = document.createElement('ol');
-    anwserList.setAttribute('id', 'anwser-list');
-    // Create the items that represent the anwsers and add them to the list
-    for (const anwserIndex in question.anwsers) {
+    var buttonList = document.createElement('ol');
+    // Create the buttons that represent the anwsers and add them to the list
+    for (const anwserIndex in currentQuestion.anwsers) {
         var anwserItem = document.createElement('button');
-        anwserItem.classList.add('anwser-item');
-        anwserItem.innerHTML = question.anwsers[anwserIndex];
+        anwserItem.classList.add('anwser-button');
+        anwserItem.innerHTML = currentQuestion.anwsers[anwserIndex];
+        anwserItem.dataset.index = anwserIndex;
 
         // Create event listeners for the buttons
-        anwserItem.addEventListener('click', function(event){
-            // Disable the buttons to prevent multiple inputs
-            for(item of document.querySelectorAll('.anwser-item')){
-                item.disabled = true;
-            }
-            // Correct anwsers
-            if(anwserIndex == question.correct){
-                // Style the button to show correct anwser
-                event.target.classList.add('right-anwser');
-            } 
-            // Incorrect anwsers
-            else {
-                // Style the button to show incorrect anwser
-                event.target.classList.add('wrong-anwser');
-                // Create a snippet to inform user what the correct anwser was
-                var correction = document.createElement('p');
-                correction.classList.add('correction');
-                correction.innerHTML = "wrongo bucko";
-                // Add the snippet to the end of the action section
-                document.querySelector('#main-action-section').appendChild(document.createElement('hr'));
-                document.querySelector('#main-action-section').appendChild(correction);
-            }
-            // Start a time interval to wait and let user view the outcome, then move to the next question
-            quizTimers.push(startTimer(1,1000,null,nextQuestion));
-        })
-        anwserList.appendChild(anwserItem);
+        anwserItem.addEventListener('click', evaluateAnwser);
+    
+        buttonList.appendChild(anwserItem);
     }
-    document.querySelector('#main-action-section').replaceChildren(anwserList);
+    return [text, buttonList];
+}
+
+function evaluateAnwser(event){
+    // For each button in the list of the target button
+    for (child of event.target.parentElement.children){
+        // Disable functionality
+        child.disabled = true;
+    }
+    
+    // Evaluate if anwser was correct and act accordingly
+    if (event.target.dataset.index == currentQuestion.correctIndex){
+        event.target.classList.add('correct-anwser');
+    } else {
+        event.target.classList.add('incorrect-anwser');
+        // Add break to end of <main>
+        mainEl.appendChild(document.createElement('hr'));
+        // Add tip for correct anwser to end of <main>
+        var tip = document.createElement('p');
+        tip.innerHTML = currentQuestion.tip;
+        tip.classList.add('tip');
+        mainEl.appendChild(tip);
+        // Deduct 10 seconds (100 tenths of a second)
+        timerEl.dataset.timeRemaining -= 100;
+        updateTimer();
+    }
+    // Pause quiz timer for 2 seconds to let user see result
+    clearInterval(quizInterval)
+    questionTimeout = setTimeout(function(){
+        // If there are no more questions to ask
+        if(unaskedQuestions.length === 0){
+            // End quiz
+            mainEl.replaceChildren(...endQuiz());
+            // Exit function early
+            return;
+        }
+        // Restart quiz timer
+        quizInterval = setInterval(function() {updateTimer();},100);
+        // Set next question
+        mainEl.replaceChildren(...nextQuestion());
+    }, 2000);
 }
 
 function endQuiz(){
-    console.log('ending quiz');
-    clearInterval(...quizTimers);
-    var score = document.querySelector('#quiz-timer').innerHTML;
-    document.querySelector('#main-text-section').innerHTML = `That's the end of the quiz<br>Your final score was: ${score}`;
+    // Calculate user score
+    var score = timerEl.dataset.timeRemaining * 10;
+    
+    // Tell user the results
+    var text = document.createElement('p');
+    text.innerHTML = `That's the end of the quiz<br>Your final score was: ${score}`;
 
+    // Create form to submit new highscore
     var submissionForm = document.createElement('form');
 
+    // Create form input field for user name
     var nameField = document.createElement('input');
     nameField.setAttribute('type', 'text');
     nameField.setAttribute('id', 'name-field');
     nameField.setAttribute('placeholder', 'Enter your name');
     
+    // Create form submission button
     var submitButton = document.createElement('button');
     submitButton.innerHTML = 'Submit Highscore';
     submitButton.setAttribute('data-score', score);
     submissionForm.append(nameField);
     submissionForm.append(submitButton);
-    submissionForm.addEventListener('submit', addHighscore);
-    
-    document.querySelector('#main-action-section').replaceChildren(submissionForm);
-}
-// Duration in seconds
-function startTimer(duration, stepInterval, stepCallback = null, delayedCallback = null){
-    var stepsElapsed = 0;
-    var timerInterval = setInterval(function() {
-        stepsElapsed ++;
-        // Call this function each step
-        if(stepCallback !== null){
-            // Passes information about the timer
-            stepCallback(duration, stepsElapsed);
+
+    // Submit new highscore to table
+    submissionForm.addEventListener('submit', (event) => {
+        // Prevent default submit function
+        event.preventDefault();
+
+        // Create highscore prototype from form
+        var highscore = {
+            name: nameField.value,
+            value: score
         }
-        if(stepsElapsed / (1000/stepInterval) >= duration) {
-            // Stops execution of action at set interval
-            clearInterval(timerInterval);
-            console.log(delayedCallback);
-        
-            // Call this function on last step
-            if(delayedCallback !== null){
-                delayedCallback();
+    
+        // Define index to insert new highscore
+        var index = 0
+        // Increment index until new highscore is higher than indexed score
+        for (const score of highscores) {
+            if (highscore.value <= score.value){
+                index++;
             }
         }
-    
-      }, stepInterval);
-      return timerInterval;
-}
+        // Insert new highscore at index
+        highscores.splice(index,0,highscore);
 
-// Add a new highscore to the list
-function addHighscore(event){
-    event.preventDefault();
-
-    var highscore = {
-        name: event.target[0].value,
-        value: event.target[1].dataset.score
-    }
-    console.log(highscore);
-    
-    var index = 0
-    for (const score of highscores) {
-        if (highscore.value <= score.value){
-            index++;
-        }
-    }
-    highscores.splice(index,0,highscore);
-    
-    saveHighscores();
-    updateHighscoreTable();
-    mainIntro();
+        // Save highscores to local storage
+        saveHighscores();
+        // Update highscore table
+        updateHighscoreTable();
+        // Return to intro scene
+        mainEl.replaceChildren(...introScene());
+        })
+        return [text, submissionForm]
 }
 
 // Load highscore data from local storage and save it to highscores variable
@@ -248,12 +251,11 @@ function saveHighscores(){
 }
 
 function init(){
-    console.log('init');
-    createHeader();
-    createBody();
-
+    // Load highscores stored in local storage
     loadHighscores();
-    mainIntro();
+
+    // Set <main> element to intro scene
+    mainEl.replaceChildren(...introScene());
 }
 
 init()
